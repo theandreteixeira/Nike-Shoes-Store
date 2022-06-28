5d71a0fafe1d5f97c57f9f39fff0520177f176ba
Andre Teixeira <andreteixeira628@gmail.com>
2022-06-27 21:27:49 -0300

    create a repository

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'nike_shoes_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nike Shoes Store',
+      home: Theme(
+        data: ThemeData.light(),
+        child: NikeShoesHome(),
+      ),
+    );
+  }
+}
